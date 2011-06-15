2206f446dbe7f24f58c3634c0a9bd664d73a11a3
Piotr Nowak <p.nowak2@gmail.com>
2011-06-16 00:08:56 +0200

    code cleanup.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
-/*
-$(document).keydown(function (e) {
-	  var keyCode = e.keyCode || e.which,
-	      arrow = {left: 37, up: 38, right: 39, down: 40 };
-
-	  switch (keyCode) {
-	    case arrow.up:
-	    	// up
-	    break;
-	    case arrow.down:
-	    	// down
-	    break;
-	    case arrow.left:
-	    	// left
-	    break;
-	    case arrow.right:
-	    	// right
-	    break;
-	  }
-});
-*/
+/**
+ * Flips a note
+ */
+function flipNote(note, direction){
+	note.flip({
+		direction: direction,
+		speed: 100,
+		color: note.css("background-color"),
+		onBefore: function(clone, element){
+			element.find("div.original").toggle();
+			element.find("div.translated").toggle();
+		}
+	});	
+}
