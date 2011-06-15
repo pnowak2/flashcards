1e6b9301347562788c46eb092ad197540b28ec5e
Piotr Nowak <p.nowak2@gmail.com>
2011-06-15 14:23:17 +0200

    flip for ie

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+/*
+$(document).keydown(function (e) {
+	  var keyCode = e.keyCode || e.which,
+	      arrow = {left: 37, up: 38, right: 39, down: 40 };
+
+	  switch (keyCode) {
+	    case arrow.up:
+	    	// up
+	    break;
+	    case arrow.down:
+	    	// down
+	    break;
+	    case arrow.left:
+	    	// left
+	    break;
+	    case arrow.right:
+	    	// right
+	    break;
+	  }
+});
+*/
