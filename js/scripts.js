8b8344770070a7c22b3c74b2b4f942ee7ee13848
Piotr Nowak <p.nowak2@gmail.com>
2011-06-16 08:04:18 +0200

    tidying up

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,53 @@
+$(function(){
+	
+	/*
+	 * Set slides container width to the card width
+	 */
+	$(".slides_container").width($(".memocard:first").outerWidth()).css("visibility", "visible");
+	
+	/*
+	 * Keyboard navigation
+	 */
+	$(document).keydown(function (e) {
+		  var keyCode = e.keyCode || e.which,
+		      arrow = {left: 37, up: 38, right: 39, down: 40 };
+
+		  switch (keyCode) {
+		    case arrow.up:
+				flipNote($(".memocard:visible:last"), "bt");
+		    break;
+		    case arrow.down:
+		    	flipNote($(".memocard:visible:last"), "tb");
+		    break;
+		    case arrow.left:
+		    	$(".prev").click();
+		    break;
+		    case arrow.right:
+		    	$(".next").click();
+		    break;
+		  }
+	});
+	
+	/*
+	 * Mouse flip
+	 */
+	$(".memocard").each(function(){
+		$(this).click(function(){
+			flipNote($(this), "tb");
+		});
+	});
+	
+	/*
+	 * Slide navigation
+	 */
+	$("#slides").slides({
+		generatePagination: false,
+		generateNextPrev: true,
+		slideSpeed: 250,
+		effect: 'slide, fade'
+	});
+});
+
 /**
  * Flips a note
  */
